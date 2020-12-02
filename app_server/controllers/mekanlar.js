4562ab409ab976523c912b9838d73214ed634740
Aras <43793683+balistis@users.noreply.github.com>
2020-12-02 16:10:28 +0300

    ikinci

---
+++ app_server/controllers/mekanlar.js
@@ -0,0 +1,95 @@
+const anaSayfa=function(req, res, next) {
+  res.render('mekanlar-liste', 
+  	{ 
+  		'baslik': 'Anasayfa',
+  	'sayfaBaslik':{
+  		'siteAd':'Mekan32',
+  		'aciklama':'Tekirdag daki Tum Dukkanlar!'
+  	},
+  	'mekanlar':[
+  	{
+  	  'ad':'Ekkim',
+  	  'adres':'Sanayi Sitesi Şarap Fabrikası Yolu No:63',
+  	  'puan':5,
+  	  'imkanlar':['yapi','malzeme','baba'],
+  	  'mesafe':'1km'
+  	},
+  	{
+  	  'ad':'Saras',
+  	  'adres':'Sanayi Sitesi Şarap Fabrikası Yolu No:63',
+  	  'puan':5,
+  	  'imkanlar':['yapi','malzeme','baba'],
+  	  'mesafe':'2km'
+  	},
+  	{
+  	  'ad':'Uzay',
+  	  'adres':'Sanayi Sitesi Şarap Fabrikası Yolu No:63',
+  	  'puan':5,
+  	  'imkanlar':['yapi','malzeme','baba'],
+  	  'mesafe':'3km'
+  	},
+  	{
+  	  'ad':'Salcano',
+  	  'adres':'Sanayi Sitesi Şarap Fabrikası Yolu No:63',
+  	  'puan':5,
+  	  'imkanlar':['yapi','malzeme','baba'],
+  	  'mesafe':'4km'
+  	},
+  	{
+  	  'ad':'Sporcum',
+  	  'adres':'Sanayi Sitesi Şarap Fabrikası Yolu No:63',
+  	  'puan':5,
+  	  'imkanlar':['yapi','malzeme','baba'],
+  	  'mesafe':'5km'
+  	},
+  	]
+  }
+  );
+}
+
+const mekanBilgisi=function(req, res) {
+  res.render('mekan-detay',{ 
+  	'baslik':'Mekan Bilgisi',
+  	'sayfaBaslik':'Ekkim',
+  	'mekanBilgisi':{
+      'ad':'Ekkim',
+  	  'adres':'Sanayi Sitesi Şarap Fabrikası Yolu No:63',
+  	  'puan':5,
+  	  'imkanlar':['yapi','malzeme','baba'],
+  	  'koordinatlar':{
+  	  	'enlem':54.68023,
+  	  	'boylam':25.28025
+  	  },
+  	  'saatler':[
+        {
+      	 'gunler':'Pazartesi-Cuma',
+      	 'acilis':'5:00',
+      	 'kapanis':'22:00',
+      	 'kapali':false
+        },
+        {
+      	 'gunler':'Pazar',
+         'kapali':true
+        }
+  	  ],
+  	  'yorumlar':[
+  	    {
+  	  	'yorumYapan': 'Aras Ateser',
+  	  	'puan':5,
+  	  	'tarih': '1 Aralik 2020',
+  	  	'yorumMetni':'Harika'
+  	    }
+  	 ]
+  	}
+  });
+}
+
+const yorumEkle=function(req, res, next) {
+  res.render('yorum-ekle', { title: 'Yorum Ekle' });
+}
+
+module.exports={
+	anaSayfa,
+	mekanBilgisi,
+	yorumEkle
+}
